dd80eb7565a57caaa3df0e219468e9c49b2e68b3
han-haocheng <1849397656@qq.com>
2026-09-05 11:33:22 +0800

    feat: 播放详情页头部新增「下载」「播放列表」「添加到」按钮（左右两套头部布局均接入）

--- src/renderer/components/layout/PlayDetail/ControlBtnsLeftHeader.vue
+++ src/renderer/components/layout/PlayDetail/ControlBtnsLeftHeader.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div(:class="$style.header")
   div(ref="dom_btns" :class="$style.controBtn")
+    header-actions
     button(type="button" :class="$style.hide" :aria-label="$t('player__hide_detail_tip')" ignore-tip :title="$t('player__hide_detail_tip')" @click="hide")
       svg(:class="$style.controBtnIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="80%" viewBox="0 0 30.727 30.727" space="preserve")
         use(xlink:href="#icon-window-hide")
@@ -23,6 +24,7 @@ import { ref, onMounted, onBeforeUnmount, useCssModule } from '@common/utils/vue
 import { isFullscreen } from '@renderer/store'
 import { setShowPlayerDetail } from '@renderer/store/player/action'
 import { closeWindow, minWindow, setFullScreen } from '@renderer/utils/ipc'
+import HeaderActions from './components/HeaderActions.vue'
 
 const dom_btns = ref()
 
--- src/renderer/components/layout/PlayDetail/ControlBtnsRightHeader.vue
+++ src/renderer/components/layout/PlayDetail/ControlBtnsRightHeader.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div(:class="$style.header")
   div(ref="dom_btns" :class="$style.controBtn")
+    header-actions
     button(ref="dom_hide_btn" type="button" :class="$style.hide" :aria-label="$t('player__hide_detail_tip')" ignore-tip :title="$t('player__hide_detail_tip')" @click="hide")
       svg(:class="$style.controBtnIcon" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" height="35%" viewBox="0 0 30.727 30.727" space="preserve")
         use(xlink:href="#icon-window-hide")
@@ -23,6 +24,7 @@ import { onMounted, onBeforeUnmount, ref, useCssModule } from '@common/utils/vue
 import { isFullscreen } from '@renderer/store'
 import { setShowPlayerDetail } from '@renderer/store/player/action'
 import { closeWindow, minWindow, setFullScreen } from '@renderer/utils/ipc'
+import HeaderActions from './components/HeaderActions.vue'
 
 const dom_btns = ref()
 const cssModule = useCssModule()
