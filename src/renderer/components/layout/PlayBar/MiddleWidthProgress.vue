ea48698b0a4f7f1448fb2206580905bab7bf4ac0
han-haocheng <1849397656@qq.com>
2026-09-05 15:19:29 +0800

    style: 控制栏三区对称——播放控制（上/播/下）移至中部并 margin-auto 居中，时间与工具组（添加/下载/歌词/音量/模式/列表）统一置于右侧；同时还原 FullWidth 意外删除的时间/工具块

--- src/renderer/components/layout/PlayBar/MiddleWidthProgress.vue
+++ src/renderer/components/layout/PlayBar/MiddleWidthProgress.vue
@@ -10,16 +10,6 @@
       </div>
       <div :class="$style.status">{{ statusText }}</div>
     </div>
-    <div :class="$style.timeContent">
-      <span>{{ nowPlayTimeStr }}</span>
-      <div :class="$style.progress">
-        <common-progress-bar v-if="!isShowPlayerDetail" :class-name="$style.progressBar" :progress="progress" :handle-transition-end="handleTransitionEnd" :is-active-transition="isActiveTransition" />
-      </div>
-      <!-- <span style="margin: 0 1px;">/</span> -->
-      <span>{{ maxPlayTimeStr }}</span>
-    </div>
-    <!-- <play-progress /> -->
-    <control-btns />
     <div :class="$style.playBtnContent">
       <div :class="$style.playBtn" :aria-label="$t('player__prev')" @click="playPrev()">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" height="100%" viewBox="0 0 1024 1024" space="preserve">
@@ -40,6 +30,17 @@
         </svg>
       </div>
     </div>
+    <div :class="$style.timeContent">
+      <span>{{ nowPlayTimeStr }}</span>
+      <div :class="$style.progress">
+        <common-progress-bar v-if="!isShowPlayerDetail" :class-name="$style.progressBar" :progress="progress" :handle-transition-end="handleTransitionEnd" :is-active-transition="isActiveTransition" />
+      </div>
+      <!-- <span style="margin: 0 1px;">/</span> -->
+      <span>{{ maxPlayTimeStr }}</span>
+    </div>
+    <!-- <play-progress /> -->
+    <control-btns />
+
   </div>
 </template>
 
